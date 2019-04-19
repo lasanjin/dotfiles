c9a5fdf5c80a44f1e8290cdeb6d51a0492bceb40
lasanjin <sanjinslavnic@gmail.com>
2019-04-19 10:50:09 +0200

    Updates bash files & scripts

--- .bash_aliases
+++ .bash_aliases
@@ -1,8 +1,11 @@
+# - - - - - - - - - - - ALIASES - - - - - - - - - - - -
 #bash
+alias cbasha='code ~/.bash_aliases'
 alias cbash='code ~/.bashrc'
+alias nbasha='sudo nano ~/.bash_aliases'
 alias nbash='sudo nano ~/.bashrc'
 
-#sudo vscode 
+#sudo vscode
 alias scode='sudo code --user-data-dir="~/.vscode-root"'
 
 #reload bash
@@ -15,17 +18,6 @@ alias open="sudo xdg-open"
 alias sleep="sudo systemctl suspend"
 alias die="sudo shutdown -h now"
 
-#bluetooth
-blue () {
-cd ~/../../etc/init.d/ && sudo bluetooth "$1" && cd ~
-}
-
-#mint note
-note () {
-RB="\n"
-sudo echo -e "\n\n$1\n$2" >> ~/Documents/mint\ setup
-}
-
 #change directory
 alias ..="cd ../../"
 alias ...="cd ../../../"
@@ -34,7 +26,7 @@ alias dev="cd ~/devel/github"
 #tree
 alias t='sudo tree'
 
-#generate 20 random chars 
+#generate 20 random chars
 alias getpass="openssl rand -base64 20"
 
 #calculator
@@ -46,20 +38,90 @@ alias now='date +%T'
 #external ip address
 alias ipe='curl ipinfo.io/ip'
 
+#gnome-calendar
+alias calendar='run gnome-calendar'
+
+#intellij
+alias intellij='echo "Starting IntelliJ..." && 
+. ~/devel/idea-IU-183.5912.21/bin/idea.sh &>/dev/null &'
+
+#empty trash
+alias etrash="sudo rm -rf ~/.local/share/Trash/files*"
+
+# - - - - - - - - - - - FUNCTIONS - - - - - - - - - - - -
+#redshift
+red() {
+    if [ "$1" == "stop" ]; then
+        redshift -x
+    elif [[ "$1" =~ ^[0-9]+$ ]] && [ $1 -ge 20 ] && [ $1 -le 65 ] ; then
+        redshift -O $(($1*100))
+    else
+        echo "Invalid input"
+    fi
+}
+
+#clean dns
+cleandns() {
+    local dir=$(pwd)
+    cd /etc/init.d/ &&
+        sudo service dns-clean start
+    cd "$dir"
+}
+
+#run app
+run() {
+    if [ ! -z $1 ]; then
+        exec $1 &>/dev/null &
+        bg
+    else
+        echo "Invalid input"
+        return 0
+    fi
+}
+
+#bluetooth
+blue() {
+    if [ "$1" == "on" ] || [ "$1" == "off" ]; then
+        local dir=$(pwd)
+        cd ~/../../etc/init.d/ &&
+            sudo bluetooth "$1" && cd "$dir"
+    else
+        echo "Invalid input"
+        return 0
+    fi
+}
+
+#mint note
+note() {
+    code ~/devel/github/notes/linux/LINUX.md
+}
+
+#copy bash files
+copybash() {
+    sudo cp \
+        -rf {~/.weather.sh,~/.expressen.sh,~/.bash_aliases} \
+        ~/devel/github/dotfiles
+}
+
+#uninstall app
+remove() {
+    sudo apt-get --purge remove $1 && sudo apt-get autoremove
+}
+
 #weather
 w() {
-source ~/.weather.sh
-w $1 $2	
+    . ~/.weather.sh
+    w $1 $2
 }
 
 #weather
 weather() {
-source ~/.weather.sh
-weather $1
+    . ~/.weather.sh
+    weather $1
 }
 
 #expressen lunch
 lunch() {
-source ~/.expressen.sh
-lunch $1 $2
+    . ~/.expressen.sh
+    lunch $1 $2
 }
--- .expressen.sh
+++ .expressen.sh
@@ -1,4 +1,82 @@
- #!/bin/bash
+#!/bin/bash
+
+#expressen menu
+#$1: <#days from today>
+#$2: <language>, s for swedish, default is english
+lunch() {
+	#number of days from today
+	local ndays=0
+
+	#check if input null
+	if [ ! -z $1 ]; then
+		ndays=$1
+
+		#check if input digit or negative
+		if ! [[ "$1" =~ ^[0-9]*$ ]] || [ $1 -lt 0 ]; then
+			echo -e "\nInvalid input\n"
+			return 0
+		fi
+	fi
+
+	local today=$(date +'%Y-%m-%d')
+	local end_date=$(date -d "$today+$ndays days" +'%Y-%m-%d')
+
+	#api url
+	expressen_api_url
+	local URL=''$url'?startDate='$today'&endDate='$end_date''
+
+	#get data
+	get_expressen_data "$URL" "$2"
+	local data=$expressen_data
+	if [ -z "$data" ]; then
+		echo -e "\nNo data\n"
+		return 0
+	fi
+
+	#store data in array
+	IFS=$'\n' read -r -a arr -d '' <<<"$data"
+
+	#styling
+	local default='\e[0m'
+	local blink='\e[39m\e[5m'
+	local green='\n\e[1m\e[32m'
+
+	local length=${#arr[@]}
+	local temp=''
+	#data is stored: [date0, meat0, date0, veg0, date1, meat1, date1, veg1, ...]
+	for ((i = 0; i < $length; i += 2)); do
+	
+		#trim citation
+		local date=${arr[i]:1:-1}
+		local food=${arr[$((i + 1))]:1:-1}
+
+		if [ "$date" != "null" ] && [ "$food" != "null" ]; then
+			if [ "$date" != "$temp" ]; then
+				if [ "$2" == "s" ]; then
+					#swedish
+					echo -e "$green$(LC_TIME=sv_SE.utf-8 date --date "$date" +'%A')$default:"
+				else
+					#english
+					echo -e "$green$(date --date "$date" +'%A')$default:"
+				fi
+
+				temp=$date
+			fi
+
+			is_it_meatballs "$food" "$2"
+			index=$index
+			end="$(echo "$ingredient" | awk '{print length}')"
+
+			if [[ ! -z "$index" ]]; then
+				echo -e "$blink${food:$index:$end}$default${food:$end}"
+			else
+				echo -e "$food"
+			fi
+		fi
+	done
+
+	echo -e ""
+}
 
 #expressen data, standard lang: eng
 get_expressen_data() {
@@ -17,7 +95,7 @@ expressen_api_url() {
 	url='http://carbonateapiprod.azurewebsites.net/api/'$api''
 }
 
-#return index if string contains 'MEATBALLS' or 'KöTTBULLAR' 
+#return index if string contains 'MEATBALLS' or 'KöTTBULLAR'
 is_it_meatballs() {
 	ingredient='MEATBALLS'
 	if [ "$2" == "s" ]; then
@@ -26,86 +104,4 @@ is_it_meatballs() {
 
 	local capital="$(echo "$1" | tr a-z A-Z)"
 	index="$(echo "$capital" | grep -b -o "$ingredient" | awk 'BEGIN {FS=":"}{print $1}')"
-}
-
-#expressen menu 
-#$1: £days from today
-#$2: language swe/eng, default is swe
-lunch() {
-#number of days from today
-local ndays=0
-
-#check if input null 
-if [ ! -z $1 ]; then
-	ndays=$1
-
-	#check if input digit or negative
-	if ! [[ "$1" =~ ^[0-9]*$ ]] || [ $1 -lt 0 ]; then
-		echo -e "\nInvalid input\n"
-		return 0
-	fi
-
-fi
-
-local today=$(date +'%Y-%m-%d')
-local end_date=$(date -d "$today+$ndays days" +'%Y-%m-%d')
-
-#api url
-expressen_api_url
-local URL=''$url'?startDate='$today'&endDate='$end_date''
-
-#get data
-get_expressen_data "$URL" "$2"
-local data=$expressen_data
-if [ -z "$data" ]; then
-	echo -e "\nNo data\n"
-	return 0
-fi
-	
-#store data in array
-IFS=$'\n' read -r -a arr -d '' <<< "$data"
-
-#styling
-local default='\e[0m'
-local blink='\e[39m\e[5m'
-local green='\n\e[1m\e[32m'
-
-local length=${#arr[@]}
-local temp=''
-#data is stored: [date0, meat0, date0, veg0, date1, meat1, date1, veg1, ...]
-for ((i=0; i<$length; i+=2))
-do
-	#trim citation
-	local date=${arr[i]:1:-1}
-	local food=${arr[$((i+1))]:1:-1}
-
-if [ "$date" != "null" ] && [ "$food" != "null" ]; then
-	if [ "$date" != "$temp" ]; then
-		
-		
-		if [ "$2" == "s" ]; then
-			#swedish
-			echo -e "$green$(LC_TIME=sv_SE.utf-8 date --date "$date" +'%A')$default:"
-		else
-			#english
-			echo -e "$green$(date --date "$date" +'%A')$default:"
-		fi
-
-		temp=$date
-	fi
-
-	#is it meatballs?
-	is_it_meatballs "$food" "$2"
-	index=$index
-	end="$(echo "$ingredient" | awk '{print length}')"
-	
-	if [[ ! -z "$index" ]]; then
-		
-		echo -e "$blink${food:$index:$end}$default${food:$end}"
-	else
-		echo -e "$food"
-	fi
-fi
-done
-echo -e ""
 }
